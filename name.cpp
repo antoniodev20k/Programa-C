a3a777ab3b736f3c179c2e53fb998ba3bcc38082
antoniodev20k <antoniochiquina@gmail.com>
2025-07-27 10:35:04 +0100

    V25.8

---
+++ name.cpp
@@ -0,0 +1,13 @@
+//write and read your name!
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"Enter your first name please: \n";
+    string first_name;
+    cin >> first_name;
+    cout <<"Hello, " << first_name <<"!\n";
+    return 0;
+}
+
+
